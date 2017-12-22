541e2b326bf3772572fbde42ae677ba5a73f1d6f
Jim Long <jimlong@antimatter-spare.local>
2017-12-21 20:15:09 -0800

    Add image Cache (RAM only for now)

--- AppleInterview/Model/Video.swift
+++ AppleInterview/Model/Video.swift
@@ -16,6 +16,9 @@ enum Provider: String {
 }
 
 class Video {
+    
+    static let imageCache = NSCache<NSString, NSData>()
+    
     var title: String
     var imageUrl: String
     var provider = Provider.unknown
@@ -30,20 +33,35 @@ class Video {
     
     func getImage(completion: (UIImage?) -> ()){
         
+        if let cachedData = Video.imageCache.object(forKey: imageUrl as NSString) as Data? {
+            let image = UIImage(data: cachedData)
+            completion(image)
+            return
+        }
+        
         switch provider {
         case Provider.apple:
             // apple code to get url
             let image = #imageLiteral(resourceName: "apple")
+            if let data = UIImagePNGRepresentation(image) as NSData? {
+                Video.imageCache.setObject(data, forKey: imageUrl as NSString)
+            }
             //  replace code above with code to get image from Apple
             completion(image)
         case Provider.hbo:
             // hbo code to get url
             let image = #imageLiteral(resourceName: "hbo")
+            if let data = UIImagePNGRepresentation(image) as NSData? {
+                Video.imageCache.setObject(data, forKey: imageUrl as NSString)
+            }
             //  replace code above with code to get image from HBO
             completion(image)
         case Provider.amazon:
             // amazon code to get url
             let image = #imageLiteral(resourceName: "amazon")
+            if let data = UIImagePNGRepresentation(image) as NSData? {
+                Video.imageCache.setObject(data, forKey: imageUrl as NSString)
+            }
             //  replace code above with code to get image from Amazon
             completion(image)
         case Provider.unknown:
